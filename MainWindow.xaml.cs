bb06813ede35789eb5dc4d01a74ec0e1df9119aa
Kamv'elihle Dyantyi <162297812+KamvelihleAthabileDyantyi17@users.noreply.github.com>
2025-04-25 18:00:25 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RoomRentalAppilcationForm
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -21,14 +10,37 @@ namespace RoomRentalAppilcationForm
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
-
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Foreground == Brushes.Gray)
+            {
+                textBox.Text = "";
+                textBox.Foreground = Brushes.Black;
+            }
         }
 
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
         {
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                // Restore placeholder text based on which textbox it is
+                if (textBox == txtFirstName) textBox.Text = "First";
+                else if (textBox == txtLastName) textBox.Text = "Last";
+                else if (textBox == txtPhoneNumber) textBox.Text = "Phone number";
+                else if (textBox == txtAddress) textBox.Text = "Address";
+                else if (textBox == txtServerAddress) textBox.Text = "Server Address";
+                else if (textBox == txtCity) textBox.Text = "City";
+                
+                textBox.Foreground = Brushes.Gray;
+            }
+        }
 
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Add validation logic here
+            MessageBox.Show("Application submitted!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
